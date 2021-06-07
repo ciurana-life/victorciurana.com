e5d23d7fceecb080bc921fad35c607ceea333b1f
Victor <admin@victorciurana.com>
2021-06-07 10:05:22 +0200

    New allowed_hosts, flake8 FIX

--- app/blog_api/views.py
+++ app/blog_api/views.py
@@ -3,7 +3,6 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework import generics
 from rest_framework import viewsets
-from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 
 from app.blog.models import BlogPost, HomePageContent
